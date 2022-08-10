1223c353bbb193193586558b6f56b71b712cab77
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-08-11 00:15:41 +0600

    booking modal

--- doctors-portal-client/src/Pages/Appointment/AvailableAppointment.js
+++ doctors-portal-client/src/Pages/Appointment/AvailableAppointment.js
@@ -1,27 +1,34 @@
 import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
+import BookingModal from "./BookingModal";
 import Service from "./Service";
 
 const AvailableAppointment = ({ date }) => {
-    const [services, setServices] = useState([])
+   const [services, setServices] = useState([]);
+   const [treatment, setTreatment] = useState(null);
    const formattedDate = format(date, "PP");
 
    useEffect(() => {
-    fetch('services.json')
-    .then(response => response.json())
-    .then(data => setServices(data))
-    
-
-   }, [])
+      fetch("services.json")
+         .then((response) => response.json())
+         .then((data) => setServices(data));
+   }, []);
    return (
       <div>
          <h4 className="text-xl text-secondary text-center my-12">
             Available Appointment on {formattedDate}
          </h4>
          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {services.map(service =><Service key={service._id} service={service}></Service>)}
-            
+            {services.map((service) => (
+               <Service key={service._id} service={service} setTreatment={setTreatment}></Service>
+            ))}
          </div>
+         {treatment && <BookingModal
+                date={date}
+                treatment={treatment}
+                setTreatment={setTreatment}
+            ></BookingModal>}
+
       </div>
    );
 };
--- doctors-portal-client/src/Pages/Appointment/Service.js
+++ doctors-portal-client/src/Pages/Appointment/Service.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import PrimaryButton from '../Shared/PrimaryButton';
+import React from "react";
 
-const Service = ({service}) => {
-
-    const { name , slots } = service
-    return (
-        <div class="card w-lg bg-base-100 shadow-xl">
-               <div class="card-body">
-                  <h2 class="card-title text-xl font-bold text-secondary">{name}</h2>
-                  <p className="uppercase"> {slots.length} {slots.length ? <span className="">spaces available</span>: <span className="text-red-500">space available</span> }</p>
-                  <div class="card-actions justify-center">
-                    <PrimaryButton>Book Appointment</PrimaryButton>
-                  </div>
-               </div>
+const Service = ({ service, setTreatment }) => {
+   const { name, slots } = service;
+   return (
+      <div className="card w-lg bg-base-100 shadow-xl">
+         <div className="card-body">
+            <h2 className="card-title text-xl font-bold text-secondary">
+               {name}
+            </h2>
+            <p className=" text-xs">
+               {slots.length ? (
+                  <span> </span>
+               ) : (
+                  <span className="text-red-500">Please try another date</span>
+               )}
+            </p>
+            <p className="uppercase text-xs">
+               {" "}
+               {slots.length}{" "}
+               {slots.length ? (
+                  <span>Spaces available</span>
+               ) : (
+                  <span className="text-red-500">Space available</span>
+               )}
+            </p>
+            <div className="card-actions justify-center">
+            <label
+                        htmlFor="booking-modal"
+                        disabled={slots.length === 0}
+                        onClick={() => setTreatment(service)}
+                        className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary"
+                    >Book Appointment</label>
             </div>
-    );
+         </div>
+      </div>
+   );
 };
 
 export default Service;
