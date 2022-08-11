93e46831079d025e32349555a9f7a3424bb5f743
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-08-11 21:28:38 +0600

    data get from mongodb

--- doctors-portal-server/index.js
+++ doctors-portal-server/index.js
@@ -8,20 +8,35 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// console.log(process.env)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lehtzpo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// console.log(uri)
-// console.log(process.env)
 
 
+async function run(){
+  try {
+    await client.connect();
+    
+    const serviceCollections = await client.db("doctors_portal").collection('service');
+    console.log("db connected ")
+
+    app.get('/service', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollections.find(query);
+      const service = await cursor.toArray();
+      res.send(service);      
+    })
+
+  }
+  finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello Doctors portal Server')
 })
-app.get('/about', (req, res) => {
-  res.send('Inshallah I will get a job immediately')
-})
+
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
